0e05926676b1278d1290eb1b91e422b754288236
TheGOYG <elchinsuleyman@gmail.com>
2017-04-01 17:26:43 -0400

    Add files via upload

---
+++ underwater.js
@@ -0,0 +1,38 @@
+﻿#pragma strict
+
+
+var waterLevel: float;
+private var isUnderwater: boolean;
+private var  normalColor:Color;
+private var  underwaterColor:Color;
+
+
+function Start () {
+	normalColor =new Color (0.5f,0.5f, 0.5f, 0.5f );
+	underwaterColor =new Color (0.22f, 0.65f, 0.77f, 0.2f);
+
+}
+
+function Update () {
+if ((transform.position.y < waterLevel) != isUnderwater)
+{
+isUnderwater = transform.position.y < waterLevel;
+if(isUnderwater) setUnderwater();
+if(!isUnderwater) setNormal();
+}
+}
+
+
+function setUnderwater(){
+RenderSettings.fog = true;
+RenderSettings.fogColor = underwaterColor;
+RenderSettings.fogDensity = 1.0f;
+
+}
+
+function setNormal(){
+RenderSettings.fog = true;
+RenderSettings.fogColor = normalColor;
+RenderSettings.fogDensity = 0.000f;
+
+}
